be1aaca6b9d9304f5f0581cb3af93153c2191353
BadPixxel <eshop.bpaquier@gmail.com>
2024-04-22 13:52:25 +0200

    WIP: Addresses, Class names & many more

--- src/Services/AddressUpdater.php
+++ src/Services/AddressUpdater.php
@@ -15,13 +15,15 @@
 
 namespace Splash\Connectors\Sellsy\Services;
 
-use RuntimeException;
-use Splash\Connectors\Sellsy\Models\Metadata\Addresses\Addresses;
+use Splash\Client\Splash;
+use Splash\Connectors\Sellsy\Models\Metadata\Address;
+use Splash\Connectors\Sellsy\Models\Metadata\Company;
+use Splash\Connectors\Sellsy\Models\Metadata\Contact;
 use Splash\OpenApi\Hydrator\Hydrator;
 use Splash\OpenApi\Models\Connexion\ConnexionInterface;
 
 /**
- *
+ * Manage Updates of Companies Addresses
  */
 class AddressUpdater
 {
@@ -41,35 +43,101 @@ class AddressUpdater
     }
 
     /**
-     * Create or Update Address
-     *
-     * @param null|Addresses $address
-     * @param string         $addressType
-     *
-     * @return void
+     * Create or Update Invoicing Address
      */
-    public function createOrUpdateAddress(?Addresses $address, string $addressType): void
+    public function createOrUpdateInvoicingAddress(Company|Contact $parent): void
     {
-        if (!$address) {
-            $address = new Addresses();
+        //====================================================================//
+        // Safety Check - Address is Defined
+        if (!$address = $parent->invoicingAddress) {
+            return;
         }
-        switch ($addressType) {
-            case 'invoicing':
-                if ('' === $address->name) {
-                    $address->name = 'Addresse de facturation';
-                }
-                $address->isInvoicingAddress = true;
-
-                break;
-            case 'delivery':
-                if ('' === $address->name) {
-                    $address->name = 'Addresse de livraison';
-                }
-                $address->isDeliveryAddress = true;
-
-                break;
-            default:
-                throw new RuntimeException('Invalid address type');
+        //====================================================================//
+        // Ensure Name is Defined
+        if (empty($address->name)) {
+            $address->name = "Invoicing Address";
         }
+        //====================================================================//
+        // Force Invoicing Address Flag
+        $address->isInvoicingAddress = true;
+        //====================================================================//
+        // Update Address
+        $this->createOrUpdateAddress($parent, $address);
+    }
+
+    /**
+     * Create or Update Delivery Address
+     */
+    public function createOrUpdateDeliveryAddress(Company|Contact $parent): void
+    {
+        //====================================================================//
+        // Safety Check - Address is Defined
+        if (!$address = $parent->deliveryAddress) {
+            return;
+        }
+        //====================================================================//
+        // Safety Check - Addresses are NOT Similar
+        if ($this->isBothInvoicingAndDelivery($parent)) {
+            Splash::log()->msg(
+                "Invoicing and Delivery Address are similar, update of delivery address skipped"
+            );
+
+            return;
+        }
+        //====================================================================//
+        // Ensure Name is Defined
+        if (empty($address->name)) {
+            $address->name = "Delivery Address";
+        }
+        //====================================================================//
+        // Force Invoicing Address Flag
+        $address->isDeliveryAddress = true;
+        //====================================================================//
+        // Update Address
+        $this->createOrUpdateAddress($parent, $address);
+    }
+
+    /**
+     * Create or Update Address
+     */
+    private function createOrUpdateAddress(Company|Contact $parent, Address $address): void
+    {
+        if (empty($address->id)) {
+            //====================================================================//
+            // Create Address
+            $this->connexion->post(
+                $this->getBaseUri($parent),
+                $this->hydrator->extract($address)
+            );
+        } else {
+            //====================================================================//
+            // Update Address
+            $this->connexion->put(
+                sprintf("%s/%d", $this->getBaseUri($parent), $address->id),
+                $this->hydrator->extract($address)
+            );
+        }
+    }
+
+    /**
+     * Build Addresses Base Uri
+     */
+    private function getBaseUri(Company|Contact $parent): string
+    {
+        return match ($parent::class) {
+            Company::class => sprintf("/companies/%d/addresses", $parent->id),
+            Contact::class => sprintf("/contacts/%d/addresses", $parent->id),
+        };
+    }
+
+    /**
+     * Check if Company Addresses are Similar
+     */
+    private function isBothInvoicingAndDelivery(Company|Contact $parent): bool
+    {
+        $invoicingId = $parent->invoicingAddress?->id ?? null;
+        $deliveryId = $parent->deliveryAddress?->id ?? null;
+
+        return $invoicingId && $deliveryId && ($invoicingId == $deliveryId);
     }
 }

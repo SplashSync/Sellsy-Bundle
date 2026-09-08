04e407a1e48f11c15596f21b189ed788e531c215
BadPixxel <eshop.bpaquier@gmail.com>
2026-09-08 18:07:01 +0200

    UPD: Migrate migration fixtures to plain SQL for Symfony 7 compatibility, refine volume setup in Docker, and enhance Sellsy configurations

--- docker/sandbox/Migrations/Version202407TaxeFixtures.php
+++ docker/sandbox/Migrations/Version202407TaxeFixtures.php
@@ -13,119 +13,55 @@
  *  file that was distributed with this source code.
  */
 
-/*
- *  This file is part of SplashSync Project.
- *
- *  Copyright (C) Splash Sync  <www.splashsync.com>
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *
- *  For the full copyright and license information, please view the LICENSE
- *  file that was distributed with this source code.
- */
-
 namespace DoctrineMigrations;
 
-// phpcs:disable Generic.Files.LineLength
-
-use App\Entity\Taxe;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\DependencyInjection\ContainerAwareInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Populate Default FR Tax IDs
+ *
+ * Written as plain SQL: ContainerAwareInterface, which the previous version
+ * used to reach the entity manager, was removed in Symfony 7.
  */
-final class Version202407TaxeFixtures extends AbstractMigration implements ContainerAwareInterface
+final class Version202407TaxeFixtures extends AbstractMigration
 {
-    private ContainerInterface $container;
+    /**
+     * Default French VAT rates
+     *
+     * @var array<string, float>
+     */
+    private const TAXES = array(
+        "VATFR20" => 20.0,
+        "VATFR10" => 10.0,
+        "VATFR55" => 5.5,
+        "VATFR0" => 0.0,
+    );
+
     public function getDescription(): string
     {
         return 'Sellsy Sandbox: Populate Default FR Tax IDs';
     }
 
     /**
-     * Inject Container
-     */
-    public function setContainer(ContainerInterface $container = null): void
-    {
-        $this->container = $container;
-    }
-
-    /**
-     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
-     * @SuppressWarnings(PHPMD.ShortMethodName)
+     * @SuppressWarnings(UnusedFormalParameter)
+     * @SuppressWarnings(ShortMethodName)
      */
     public function up(Schema $schema): void
     {
-    }
-
-    /**
-     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
-     */
-    public function postUp(Schema $schema): void
-    {
-        /** @var EntityManagerInterface $em */
-        $em = $this->container->get('doctrine.orm.entity_manager');
-
-        foreach ($this->getTaxes() as $taxe) {
-            if (!$em->contains($taxe)) {
-                $em->persist($taxe);
-            }
+        foreach (self::TAXES as $label => $rate) {
+            $this->addSql(
+                "INSERT INTO taxe (rate, label, is_active, is_ecotax) VALUES (:rate, :label, 1, 0)",
+                array("rate" => $rate, "label" => $label)
+            );
         }
-
-        $em->flush();
     }
 
     /**
-     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     * @SuppressWarnings(UnusedFormalParameter)
      */
     public function down(Schema $schema): void
     {
-    }
-
-    /**
-     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
-     */
-    public function postDown(Schema $schema): void
-    {
-        /** @var EntityManagerInterface $em */
-        $em = $this->container->get('doctrine.orm.entity_manager');
-
-        foreach ($em->getRepository(Taxe::class)->findAll() as $taxe) {
-            $em->remove($taxe);
-        }
-
-        $em->flush();
-    }
-
-    /**
-     * @return Taxe[]
-     */
-    private function getTaxes(): array
-    {
-        $taxes = array();
-
-        $vatFr20 = new Taxe();
-        $vatFr20->rate = 20.0;
-        $vatFr20->label = "VATFR20";
-
-        $vatFr10 = new Taxe();
-        $vatFr10->rate = 10.0;
-        $vatFr10->label = "VATFR10";
-
-        $vatFr55 = new Taxe();
-        $vatFr55->rate = 5.5;
-        $vatFr55->label = "VATFR55";
-
-        $vatFr0 = new Taxe();
-        $vatFr0->rate = 0.0;
-        $vatFr0->label = "VATFR0";
-
-        return array($vatFr20, $vatFr10, $vatFr55, $vatFr0, $vatFr55);
+        $this->addSql("DELETE FROM taxe");
     }
 }
--- docker/sandbox/Migrations/Version202412PaymentMethodsFixtures.php
+++ docker/sandbox/Migrations/Version202412PaymentMethodsFixtures.php
@@ -13,111 +13,54 @@
  *  file that was distributed with this source code.
  */
 
-/*
- *  This file is part of SplashSync Project.
- *
- *  Copyright (C) Splash Sync  <www.splashsync.com>
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *
- *  For the full copyright and license information, please view the LICENSE
- *  file that was distributed with this source code.
- */
-
 namespace DoctrineMigrations;
 
-// phpcs:disable Generic.Files.LineLength
-
-use App\Entity\PaymentMethod;
-use App\Entity\Taxe;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\DependencyInjection\ContainerAwareInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * Populate Default Payment Methods IDs
+ * Populate Default Payment Methods
+ *
+ * Written as plain SQL: ContainerAwareInterface, which the previous version
+ * used to reach the entity manager, was removed in Symfony 7.
  */
-final class Version202412PaymentMethodsFixtures extends AbstractMigration implements ContainerAwareInterface
+final class Version202412PaymentMethodsFixtures extends AbstractMigration
 {
-    private ContainerInterface $container;
+    /**
+     * Sandbox Payment Methods
+     *
+     * @var string[]
+     */
+    private const METHODS = array(
+        "JohnnyCash",
+        "WalkerBank",
+        "DrunkFood",
+    );
+
     public function getDescription(): string
     {
-        return 'Sellsy Sandbox: Populate Default Payment Methods IDs';
+        return 'Sellsy Sandbox: Populate Default Payment Methods';
     }
 
     /**
-     * Inject Container
-     */
-    public function setContainer(ContainerInterface $container = null): void
-    {
-        $this->container = $container;
-    }
-
-    /**
-     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
-     * @SuppressWarnings(PHPMD.ShortMethodName)
+     * @SuppressWarnings(UnusedFormalParameter)
+     * @SuppressWarnings(ShortMethodName)
      */
     public function up(Schema $schema): void
     {
-    }
-
-    /**
-     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
-     */
-    public function postUp(Schema $schema): void
-    {
-        /** @var EntityManagerInterface $em */
-        $em = $this->container->get('doctrine.orm.entity_manager');
-
-        foreach ($this->getMethods() as $taxe) {
-            if (!$em->contains($taxe)) {
-                $em->persist($taxe);
-            }
+        foreach (self::METHODS as $label) {
+            $this->addSql(
+                "INSERT INTO payment_method (label) VALUES (:label)",
+                array("label" => $label)
+            );
         }
-
-        $em->flush();
     }
 
     /**
-     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     * @SuppressWarnings(UnusedFormalParameter)
      */
     public function down(Schema $schema): void
     {
-    }
-
-    /**
-     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
-     */
-    public function postDown(Schema $schema): void
-    {
-        /** @var EntityManagerInterface $em */
-        $em = $this->container->get('doctrine.orm.entity_manager');
-
-        foreach ($em->getRepository(Taxe::class)->findAll() as $taxe) {
-            $em->remove($taxe);
-        }
-
-        $em->flush();
-    }
-
-    /**
-     * @return PaymentMethod[]
-     */
-    private function getMethods(): array
-    {
-        $method1 = new PaymentMethod();
-        $method1->label = "JohnnyCash";
-
-        $method2 = new PaymentMethod();
-        $method2->label = "WalkerBank";
-
-        $method3 = new PaymentMethod();
-        $method3->label = "DrunkFood";
-
-        return array($method1, $method2, $method3);
+        $this->addSql("DELETE FROM payment_method");
     }
 }
